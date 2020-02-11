a603dec92427d9438e77388f416ae5b176f90c47
Gregory Losco <gtlosco@gmail.com>
2020-02-11 23:28:17 +0000

    slug & find_by_slug  methods added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base 
   has_secure_password
   has_many :shoes 
+  
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+  
+  def self.find_by_slug(slug)
+    User.all.find do |user|
+      user.slug == slug
+    end
+  end
+  
 end
