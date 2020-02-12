0c6c79782f46169f16bec3acbc536092ac6b7353
Gregory Losco <gtlosco@gmail.com>
2020-02-12 01:03:52 +0000

    added shoes get route and shoes.erb

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,2 +1,12 @@
 class ShoesController < ApplicationController 
+  
+   get '/shoes' do 
+    if logged_in?
+      @shoes = Shoe.all
+      erb :'/shoes/shoes'
+    else
+      redirect to '/login'
+    end
+  end
+  
 end
