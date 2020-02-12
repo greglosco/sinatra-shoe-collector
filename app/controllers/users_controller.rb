e5b084e74d13883233aab8b301c1a3ccfe4bd678
Gregory Losco <gtlosco@gmail.com>
2020-02-12 00:57:16 +0000

    logout route completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class UsersController < ApplicationController
   
   get '/signup' do 
@@ -46,5 +48,14 @@ class UsersController < ApplicationController
       redirect to '/login'
     end
   end
+  
+  get "/logout" do
+	  if logged_in?
+		  session.clear
+		  redirect to "/login"
+		else 
+		  redirect to '/'
+		end
+	end
   
 end
